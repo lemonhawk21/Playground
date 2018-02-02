255628c6ceb07f10e93d3ce6dcba668ac86fdc0e
Adam Miller <ammiller@linkedin.com>
2018-02-02 12:09:52 -0800

    Added homeworks 1, 2 and 3

---
+++ homework/hw2.js
@@ -0,0 +1,54 @@
+startHomework();
+
+// # If Else Statements
+// These exercises test your knowledge of conditional statements and functions.
+// Feel free to review if / else if / else statements here:
+// https://www.w3schools.com/js/js_if_else.asp
+
+problem("What number's bigger?", () => {
+
+  /******************************************************************************/
+  // What number's bigger?
+  // Write a function named greaterNum that:
+  //  - Takes 2 arguments, both numbers.
+  //  - Returns whichever number is the greater (higher) number.
+  // Call that function 2 times with different number pairs, and log the output to
+  // make sure it works (e.g. "The greater number of 5 and 10 is 10.").
+  /******************************************************************************/
+
+  // <Code Goes Here>
+
+
+});
+
+problem("The Grade Assigner", () => {
+
+  /******************************************************************************/
+  // The Grade Assigner
+  // Write a function named assignGrade that:
+  //  - Takes 1 argument, a number score.
+  //  - Returns a grade for the score, either "A", "B", "C", "D", or "F".
+  // Call that function for a few different scores and log the result to make sure it works.
+  /******************************************************************************/
+
+  // <Code Goes Here>
+
+
+});
+
+problem("The Pluralizer", () => {
+
+
+  /******************************************************************************/
+  // The Pluralizer
+  // Write a function named pluralize that:
+  //  - Takes 2 arguments, a noun and a number.
+  //  - Returns the number and pluralized form, like "5 cats" or "1 dog".
+  // Call that function for a few different scores and log the result to make sure it works.
+  // Bonus: Make it handle a few collective nouns like "sheep" and "geese".
+  /******************************************************************************/
+
+  // <Code Goes Here>
+
+
+});
